79013903fe7b413e5b6adf93898184e2684e576c
wcarthur <wcarthur@gmail.com>
2021-12-16 21:18:22 +1100

    Push PI direct to data project

--- move_pcmin.sh
+++ move_pcmin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #PBS -q copyq
 #PBS -l walltime=10:00:00
-#PBS -l storage=scratch/w85+gdata/w85
+#PBS -l storage=scratch/w85+gdata/fj6
 #PBS -l mem=2GB
 #PBS -l wd
 #PBS -m ae
@@ -10,15 +10,17 @@
 # Craig Arthur, 2020-04-28
 # Moving pcmin data to an externally accessible endpoint
 
-cd /scratch/w85/cxa547 
-find ./pcmin -type f -print0 | xargs -0 md5sum > pcmin.md5
-cp -Rpu /scratch/w85/cxa547/pcmin/dltm/ /g/data/w85/data/pcmin/
-cp -Rpu /scratch/w85/cxa547/pcmin/annual/ /g/data/w85/data/pcmin/
-cp -Rpu /scratch/w85/cxa547/pcmin/monthly/ /g/data/w85/data/pcmin/
-cp -Rpu /scratch/w85/cxa547/pcmin/daily/ /g/data/w85/data/pcmin/
-cp -Rpu /scratch/w85/cxa547/pcmin/trend/ /g/data/w85/data/pcmin/
-cp -pu /scratch/w85/cxa547/pcmin/README.rst /g/data/w85/data/pcmin/
+cd /scratch/w85/cxa547/pcmin 
 
-cd /g/data/w85/data
-md5sum -c /scratch/w85/cxa547/pcmin.md5 > pcmin.md5check
+DEST=/g/data/fj6/PI/
+find ./ -type f -print0 | xargs -0 md5sum > pcmin.md5
+cp -Rpu /scratch/w85/cxa547/pcmin/dltm/ /g/data/fj6/PI/
+cp -Rpu /scratch/w85/cxa547/pcmin/annual/ /g/data/fj6/PI/
+cp -Rpu /scratch/w85/cxa547/pcmin/monthly/ /g/data/fj6/PI/
+cp -Rpu /scratch/w85/cxa547/pcmin/daily/ /g/data/fj6/PI/
+cp -Rpu /scratch/w85/cxa547/pcmin/trend/ /g/data/fj6/PI/
+cp -pu /scratch/w85/cxa547/pcmin/README.rst /g/data/fj6/PI/
+
+cd /g/data/fj6/PI/
+md5sum -c /scratch/w85/cxa547/pcmin/pcmin.md5 > pcmin.md5check
 
