5c5252b07dcb2a484ee5b86e7df3c223d70adcd9
wcarthur <wcarthur@gmail.com>
2024-09-17 13:12:26 +1000

    Update jpb script

--- calc_pi.sh
+++ calc_pi.sh
@@ -11,13 +11,13 @@
 #PBS -joe
 #PBS -o /home/547/cxa547/pcmin/logs/calcpi.out.log
 #PBS -e /home/547/cxa547/pcmin/logs/calcpi.err.log
-#PBS -lstorage=gdata/w85+gdata/rt52+scratch/w85
+#PBS -lstorage=gdata/w85+gdata/rt52+gdata/hh5+scratch/w85
 
 # Run this with the following command line:
 #
-# qsub -v NJOBS=42,YEAR=1979 calc_pi.sh
+# qsub -v NJOBS=44,YEAR=1979 calc_pi.sh
 #
-# This will run the process for 42 years, starting 1979
+# This will run the process for 44 years, starting 1979
 
 module purge
 module load pbs
@@ -40,16 +40,16 @@ if [ X$NJOBS == X ]; then
 else
     if [ X$NJOB == X ]; then
     $ECHO "NJOBS set to $NJOBS"
-    fi 
+    fi
 fi
-  
+
 if [ X$NJOB == X ]; then
     $ECHO "NJOB (current job number in sequence) is not set - defaulting to 1"
     export NJOB=1
 fi
 
 #
-# Quick termination of job sequence - look for a specific file 
+# Quick termination of job sequence - look for a specific file
 #
 if [ -f STOP_SEQUENCE ] ; then
     $ECHO  "Terminating sequence at job number $NJOB"
@@ -59,7 +59,7 @@ fi
 if [ X$NJOB == X1 ]; then
     $ECHO "This is the first year - it's not a restart"
     export YEAR=1979
-    
+
 else
     export YEAR=$(($YEAR+1))
 fi
